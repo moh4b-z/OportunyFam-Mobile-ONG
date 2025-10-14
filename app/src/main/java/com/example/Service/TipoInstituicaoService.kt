b80ca0b6a20ce0ac87ba365df5e91d5714f88be0
MOHAMMAD SALIM SOARES SILVA <24122313@dsn127m1242374.cfp127aluno.local>
2025-10-14 13:59:53 -0300

    arrumar urls

--- app/src/main/java/com/example/Service/TipoInstituicaoService.kt
+++ app/src/main/java/com/example/Service/TipoInstituicaoService.kt
@@ -8,23 +8,23 @@ import retrofit2.http.*
 interface TipoInstituicaoService {
 
     // POST /v1/tipoInstituicao - Criar novo tipo
-    @POST("tipoInstituicao")
+    @POST("tipoInstituicoes")
     fun criar(@Body tipoInstituicao: TipoInstituicao): Call<TipoInstituicao>
 
     // DELETE /v1/tipoInstituicao/:id - Excluir tipo por ID
-    @DELETE("tipoInstituicao/{id}")
+    @DELETE("tipoInstituicoes/{id}")
     fun deletar(@Path("id") id: Int): Call<Unit>
 
     // PUT /v1/tipoInstituicao/:id - Atualizar tipo por ID
-    @PUT("tipoInstituicao/{id}")
+    @PUT("tipoInstituicoes/{id}")
     fun atualizar(@Path("id") id: Int, @Body tipoInstituicao: TipoInstituicao): Call<TipoInstituicao>
 
     // GET /v1/tipoInstituicao - Listar todos os tipos
-    @GET("tipoInstituicao/")
+    @GET("tipoInstituicoes/")
     fun listarTodos(): Call<TipoInstituicaoResponse>
 
     // GET /v1/tipoInstituicao/:id - Buscar tipo por ID
-    @GET("tipoInstituicao/{id}")
+    @GET("tipoInstituicoes/{id}")
     fun buscarPorId(@Path("id") id: Int): Call<TipoInstituicaoResponse>
 
 
